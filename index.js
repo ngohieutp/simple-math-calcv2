0550a370e6fa3ef029ff095255ccda0db3d9e804
Ngo Hieu <ngohieutp@gmail.com>
2024-04-13 21:20:37 +0700

    add more ops

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 var ops = require('simple-calc-ops');
 
+function plus(a, b) {
+  return ops.sum(a, b);
+}
+
 function subtract(a, b) {
   return ops.minus(a, b);
 }
 
+function multiply(a, b) {
+  return ops.mul(a, b);
+}
+
+function divide(a, b) {
+  return ops.div(a, b);
+}
+
 module.exports = {
-  subtract
+	plus,
+	subtract,
+	multiply,
+	divide
 }
